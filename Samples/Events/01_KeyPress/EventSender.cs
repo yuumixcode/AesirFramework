067e75bbff66a1712deb2016a7538e82b4685b5a
yuumixcode <zeriying@gmail.com>
2026-09-06 19:57:16 +0800

    refactor: 示例程序集改为运行时参与编译、代码以 #if UNITY_EDITOR 剔除构建

--- Samples/Events/01_KeyPress/EventSender.cs
+++ Samples/Events/01_KeyPress/EventSender.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR // 示例仅编辑器内参与编译（运行时程序集保证场景可挂载，#if 保证构建剔除）
 using UnityEngine;
 
 namespace Runestone.AesirModules.Samples.Events.KeyPress
@@ -19,3 +20,4 @@ namespace Runestone.AesirModules.Samples.Events.KeyPress
         }
     }
 }
+#endif
